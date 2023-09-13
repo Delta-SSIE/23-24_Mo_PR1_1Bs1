1cd6dda3e13676e3336038298abb9f299cda4b8d
Jan Mottl <mottja@delta-skola.cz>
2023-09-13 13:00:33 +0200

    Add project files.

---
+++ 01_Uvod/01_Uvod.cs
@@ -0,0 +1,34 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Tohle je komentář – kód následující vypíše "Ahoj světe!" do konzole!
+            Console.WriteLine("Ahoj světe!");
+            Console.WriteLine("Ahoj třído!");
+
+            //Deklarace proměné
+            int vek = 5;
+            string jmeno = "Honza";
+
+            //Deklarace proměné a vložení hodnoty na novém řádku
+            int cislopasu;
+            cislopasu = 1;
+
+            //Výpis hodnot proměných
+            Console.WriteLine(vek);
+            Console.WriteLine(jmeno);
+            Console.WriteLine(cislopasu);
+
+            //Výpis hodnot proměných (více najednou)
+            Console.WriteLine(jmeno + " " + vek + " " + cislopasu);
+
+            //Součet proměných jednohot typu (s výpisem)
+            int x = 5;
+            int y = 10;
+            Console.WriteLine(x + y);
+
+        }
+    }
+}
