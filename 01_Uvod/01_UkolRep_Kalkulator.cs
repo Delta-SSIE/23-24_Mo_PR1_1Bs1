752e56a690d6ed872a022318df8f29a1ab708b3c
Jan Mottl <mottja@delta-skola.cz>
2023-09-18 10:32:45 +0200

    ok

---
+++ 01_Uvod/01_UkolRep_Kalkulator.cs
@@ -0,0 +1,21 @@
+﻿namespace Kalkulator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Odchycení vstupu od uživatele – Vybíráme float/double pro číslo s desetinou
+            Console.WriteLine("Zadej mi 1. číslo: ");
+            double prvnicislo = double.Parse(Console.ReadLine());
+            Console.WriteLine("Zadej mi 2. číslo: ");
+            double druhecislo = double.Parse(Console.ReadLine());
+
+            //Provedení matematických operací
+            Console.WriteLine("Součet čísel je " + (prvnicislo + druhecislo));
+            Console.WriteLine("Rozdíl čísel je " + (prvnicislo - druhecislo));
+            Console.WriteLine("Součin čísel je " + (prvnicislo * druhecislo));
+            Console.WriteLine("Podíl čísel je " + (prvnicislo / druhecislo));
+
+        }
+    }
+}
