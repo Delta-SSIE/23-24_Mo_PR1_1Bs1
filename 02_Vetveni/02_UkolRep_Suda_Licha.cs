bd4eed41ffe2b71b6d18e26742f0676314f87646
Jan Mottl <mottja@delta-skola.cz>
2023-10-02 09:08:56 +0200

    ok

---
+++ 02_Vetveni/02_UkolRep_Suda_Licha.cs
@@ -0,0 +1,19 @@
+﻿namespace suda_licha
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zadej číslo a já ti řeknu, jestli je sudé, či liché?");
+            int cislo = int.Parse(Console.ReadLine());
+
+            if (cislo % 2 == 0)
+            {
+                Console.WriteLine("Číslo je sudé!");
+            } else
+            {
+                Console.WriteLine("Číslo je liché");
+            }
+        }
+    }
+}
