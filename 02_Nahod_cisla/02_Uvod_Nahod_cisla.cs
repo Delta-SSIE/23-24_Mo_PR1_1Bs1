920e93316be371b46df5dcccc14117f0b972a42c
Jan Mottl <mottja@delta-skola.cz>
2023-10-09 10:21:39 +0200

    ok

---
+++ 02_Nahod_cisla/02_Uvod_Nahod_cisla.cs
@@ -0,0 +1,27 @@
+﻿namespace Nahod_cisla
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Tvorba generátoru náhodných čísel
+            Random generator_nahod = new Random();
+
+            //Generování náhodných čísel
+            Console.WriteLine(generator_nahod.Next());
+            Console.WriteLine(generator_nahod.Next());
+            Console.WriteLine(generator_nahod.Next());
+            Console.WriteLine(generator_nahod.Next());
+            Console.WriteLine(generator_nahod.Next());
+            Console.WriteLine(generator_nahod.Next());
+            Console.WriteLine(generator_nahod.Next());
+            Console.WriteLine(generator_nahod.Next());
+            Console.ReadKey();
+
+            Console.Clear();
+            //Generátor čísel s rozsahem
+            int cisloA = generator_nahod.Next(1,3);
+            Console.WriteLine(cisloA);
+        }
+    }
+}
---
+++ 02_Vetveni/02_UkolRep_JeTrojuhelník.cs
@@ -0,0 +1,42 @@
+﻿namespace trojuhelnik
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nacteno;
+
+            Console.Write("Zadej stranu A: ");
+            nacteno = Console.ReadLine();
+            double stranaA = double.Parse(nacteno);
+
+            Console.Write("Zadej stranu B: ");
+            nacteno = Console.ReadLine();
+            double stranaB = double.Parse(nacteno);
+
+            Console.Write("Zadej stranu C: ");
+            nacteno = Console.ReadLine();
+            double stranaC = double.Parse(nacteno);
+
+            // Sem přijde váš kód
+
+            bool lzeSestavit = false;
+
+            double max = Math.Max(stranaA, stranaB);
+            max = Math.Max(max, stranaC);
+            double dvejinestrany = stranaA + stranaB + stranaC - max;
+
+            if (dvejinestrany >= max)
+            {
+                lzeSestavit = true;
+            }
+
+            if (lzeSestavit)
+            {
+                Console.WriteLine("Je to trojuhleník!");
+            }
+
+            
+        }
+    }
+}
