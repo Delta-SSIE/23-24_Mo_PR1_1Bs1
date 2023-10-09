f09522f7c8d78a345b0da0edcfc6a4f96c771648
Jan Mottl <mottja@delta-skola.cz>
2023-10-09 09:34:06 +0200

    ok

---
+++ 02_Vetveni/02_UkolRep_SvatebniPoradce_I.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.InteropServices;
+
+namespace svatbeniporad
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Nastavte si proměnné
+            bool jeMlady = true;
+            bool jeHezky = false;
+            bool jeBohaty = true;
+
+            // tuto podmínku je třeba doplnit
+            if (jeHezky == true ||(jeBohaty == true && jeMlady == false))
+            // zde končí úpravy
+            {
+                Console.WriteLine("Vezmi si ho!");
+            }
+            else
+            {
+                Console.WriteLine("Toho si neber!");
+            }
+
+
+        }
+    }
+}
---
+++ 02_Vetveni/02_UkolRep_TypZnaku.cs
@@ -0,0 +1,24 @@
+﻿namespace typznaku
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Prosím stistkni tlačítko!");
+            char znak = Console.ReadKey().KeyChar;
+
+            int cislo = znak;
+
+            if (cislo >= 65 && cislo <= 90 || cislo >= 97 && cislo <= 122)
+            {
+                Console.WriteLine("Zadaný vstup je písmeno!");
+            } else if (cislo >= 48 && cislo <= 57)
+            {
+                Console.WriteLine("Zadaný vstup je číslo!");
+            } else
+            {
+                Console.WriteLine("Zadaný vstup je symbol!");
+            }
+        }
+    }
+}
