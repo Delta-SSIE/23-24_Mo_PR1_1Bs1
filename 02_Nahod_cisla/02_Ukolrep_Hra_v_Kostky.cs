15813346ab4c4166f95121161b84e73f1c99f3dd
Jan Mottl <mottja@delta-skola.cz>
2023-10-09 10:35:15 +0200

    ok

---
+++ 02_Nahod_cisla/02_Ukolrep_Hra_v_Kostky.cs
@@ -0,0 +1,28 @@
+﻿namespace hra_v_kostky
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hoď kostkou!");
+            Console.ReadKey();
+
+            //Generátor náhodného čísla
+            Random kostka = new Random();
+
+            int hod = kostka.Next(1, 7);
+            int hodsouper = kostka.Next(1, 7);
+
+            if (hod == hodsouper)
+            {
+                Console.WriteLine($"Remíza: {hod}:{hodsouper}");
+            } else if (hod < hodsouper)
+            {
+                Console.WriteLine($"Prohral jsi, {hod}:{hodsouper}");
+            } else
+            {
+                Console.WriteLine($"Vyhrál jsi, {hod}:{hodsouper}");
+            }
+        }
+    }
+}
