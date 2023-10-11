9586f8c3541d616d277d88357acb16b027102e8e
Jan Mottl <mottja@delta-skola.cz>
2023-10-11 11:49:05 +0200

    ok

---
+++ 02_Nahod_cisla/02_UkolRep_KNP_zaklad.cs
@@ -0,0 +1,26 @@
+﻿namespace KNP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random hra = new Random();
+
+            int cislo = hra.Next(1,4);
+
+            switch (cislo)
+            {
+                case 1:
+                    Console.WriteLine("Kámen");
+                    break;
+                case 2:
+                    Console.WriteLine("Nůžky");
+                    break;
+                case 3:
+                    Console.WriteLine("Papír");
+                    break;
+            }
+
+        }
+    }
+}
