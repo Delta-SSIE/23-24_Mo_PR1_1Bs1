02092d795b28ab62449af5c670bc1619d90ed56e
Jan Mottl <mottja@delta-skola.cz>
2023-10-11 12:23:49 +0200

    ok

---
+++ 02_Vetveni/02_UkolRep_Delsi_slovo.cs
@@ -0,0 +1,24 @@
+﻿namespace delka_slov
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zadej 1. slovo");
+            string slovoA = Console.ReadLine();
+            Console.WriteLine("Zadej 2. slovo");
+            string slovoB = Console.ReadLine();
+
+            if (slovoA.Length == slovoB.Length)
+            {
+                Console.WriteLine($"\"{slovoA}\" a \"{slovoB}\" jsou stejně dlouhé!");
+            } else if (slovoA.Length > slovoB.Length)
+            {
+                Console.WriteLine($"\"{slovoA}\" je delší než \"{slovoB}\"!");
+            } else
+            {
+                Console.WriteLine($"\"{slovoB}\" je delší než \"{slovoA}\"!");
+            }
+        }
+    }
+}
