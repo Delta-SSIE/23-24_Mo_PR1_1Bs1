bdb392cad4b278480425023cadfb5a30be090bc9
Jan Mottl <mottja@delta-skola.cz>
2023-10-16 10:29:22 +0200

    ok

---
+++ 02_Vetveni/02_Test_Hadej_Pismeno.cs
@@ -0,0 +1,29 @@
+﻿namespace test_hadej_pismeno
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random generator = new Random();
+            Console.WriteLine("Napiš mi slovo!");
+            string slovo = Console.ReadLine();
+
+            int delka_slova = slovo.Length;
+            int cislo = generator.Next(1, delka_slova + 1);
+
+            Console.WriteLine($"Hádej ve slově písmeno na pozici {cislo}!");
+            char pismeno = Console.ReadKey().KeyChar;
+
+            if (pismeno == slovo[cislo-1])
+            {
+                Console.WriteLine("Správně!");
+            } else
+            {
+                Console.WriteLine("Špatně");
+            }
+
+
+            
+        }
+    }
+}
