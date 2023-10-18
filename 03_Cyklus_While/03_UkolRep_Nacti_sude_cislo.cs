ddbb799038cb20e4db05b2a5fdd7e672666153c2
Jan Mottl <mottja@delta-skola.cz>
2023-10-18 12:13:25 +0200

    ok

---
+++ 03_Cyklus_While/03_UkolRep_Nacti_sude_cislo.cs
@@ -0,0 +1,21 @@
+﻿namespace Nacti_Sude
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Původní hodnota, aby nám cyklus, aspoň jednou prošel
+            int cislo = 1;
+            
+            //Podmínka cyklu je, že dokud po celočíselném dělení není zbytek 0, tak se opakuje
+            while(cislo % 2 != 0)
+            {
+                //Načte nové číslo
+                Console.WriteLine("Napiš mi číslo");
+                cislo = int.Parse(Console.ReadLine());
+            }
+            //Výpis nakonec
+            Console.WriteLine($"Napsal si sudé číslo {cislo} a nejbližší liché je {cislo+1}");
+        }
+    }
+}
