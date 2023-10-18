223f30c06bb0a31130c75df379de3b68a907756c
Jan Mottl <mottja@delta-skola.cz>
2023-10-18 12:43:18 +0200

    ok

---
+++ 03_Cyklus_While/03_UkolRep_Soucet_zadanych.cs
@@ -0,0 +1,27 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace soucet_zadanych4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            //Pomocná porměná pro ukládání součtu
+            int soucet = 0;
+
+            //Do While – nejdříve tělo, pak kontrola s podmínkou
+            do
+            {
+                Console.WriteLine("Zadej číslo");
+                x = int.Parse(Console.ReadLine());
+                //K hodnotě proměné soucet přičtu hodnotu x
+                soucet += x;
+
+            } while (x != 0);
+
+            //Výpis výsledku
+            Console.WriteLine($"Součet zadaných hodnot je {soucet}");
+        }
+    }
+}
