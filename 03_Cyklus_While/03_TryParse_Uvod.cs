de29f2c30a4f99b471baf0093e461a755ecb63c7
Jan Mottl <mottja@delta-skola.cz>
2023-10-25 12:08:43 +0200

    ok

---
+++ 03_Cyklus_While/03_TryParse_Uvod.cs
@@ -0,0 +1,29 @@
+﻿namespace TryParse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zadej číslo");
+            string txt = Console.ReadLine();
+            int cislo;
+
+            //Tryparse pokusí proměnou txt převést do proměné cislo.
+            if (int.TryParse(txt, out cislo))
+            {
+                Console.WriteLine("Zadal jsi číslo");
+            } else
+            {
+                Console.WriteLine("Zadal jsi něco jiného než číslo");
+            }
+
+
+            //TryParse v cyklu, "!" označuje negaci výrazu, dokud tedy neplatí, tak se bude cyklus opakovat!
+            while (!int.TryParse(Console.ReadLine(), out int a))
+            {
+                Console.WriteLine("Zadej číslo znovu!");
+            }
+
+        }
+    }
+}
