4b0ff3d65124864a006584f3a2ae9173064302b2
Jan Mottl <mottja@delta-skola.cz>
2023-10-25 12:47:49 +0200

    ok

---
+++ 03_Cyklus_While/03_UkolRep_Hadej_kolik_myslim.cs
@@ -0,0 +1,41 @@
+﻿namespace Hadej_kolik_myslim
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random cisla = new Random();
+            //Náhodné číslo, co si myslí
+            int cislo = cisla.Next(1, 101);
+            //hráčův odhad
+            int odhad = 0;
+            //počitadlo kol
+            int kolo = 0;
+
+            do
+            {
+                //Zvýšení počítadla o jedna.
+                kolo++;
+                Console.WriteLine($"{kolo}. Kolo");
+                Console.WriteLine("Napiš číslo: ");
+                //Cyklus na odchycení uživatelova vstupu
+                while(!int.TryParse(Console.ReadLine(),out odhad))
+                {
+                    Console.WriteLine("Zadej číslo a ne jiný datový typ!");
+                }
+                //Zjištuje jestli má přidat, či ubrat
+                if (cislo == odhad)
+                {
+                    Console.WriteLine($"Jupí hotovo! Uhádl jsi číslo v {kolo}. kole");
+                } else if (cislo < odhad)
+                {
+                    Console.WriteLine("Uber!");
+                } else
+                {
+                    Console.WriteLine("Přidej!");
+                }
+
+            } while (odhad != cislo);
+        }
+    }
+}
