01f07d01d9ef5577d34aed6708bd861f2984031f
Jan Mottl <mottja@delta-skola.cz>
2023-10-25 11:52:00 +0200

    ok

---
+++ 03_Cyklus_While/03_UkolRep_Maximum.cs
@@ -0,0 +1,31 @@
+﻿namespace Maximum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int max = 0;
+            int vstup = 0;
+
+            do
+            {
+                Console.WriteLine("Zadej mi číslo (pro ukončení zadej 0, či menší)");
+                vstup = int.Parse(Console.ReadLine());
+                if (vstup > max)
+                {
+                    max = vstup;
+                }
+
+            } while (vstup > 0);
+
+            if (max != 0)
+            {
+                Console.WriteLine($"Největší číslo je {max}");
+            } else
+            {
+                Console.WriteLine("Nebylo zadáno žádné číslo!");
+            }
+           
+        }
+    }
+}
