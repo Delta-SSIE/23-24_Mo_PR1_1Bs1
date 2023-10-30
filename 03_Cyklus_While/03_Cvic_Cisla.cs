a6adc11ceb3bc131bd9c7a38423bd9c3911ced18
Jan Mottl <mottja@delta-skola.cz>
2023-10-30 10:38:57 +0100

    ok

---
+++ 03_Cyklus_While/03_Cvic_Cisla.cs
@@ -0,0 +1,40 @@
+﻿namespace cisla
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int pocetlich = 0;
+            int pocetdsudych = 0;
+            int nejmensi = 0;
+            int nejvetsi = 0;
+            int vstup = 0;
+
+            do
+            {
+                Console.WriteLine("Zadej mi číslo, pokud chceš zadávání ukončit vlož 0");
+                while(!int.TryParse(Console.ReadLine(),out vstup))
+                {
+                    Console.WriteLine("Zadej číslo");
+                }
+                if (vstup > nejvetsi)
+                {
+                    nejvetsi = vstup;
+                } else if ((vstup < nejmensi || nejmensi == 0) && vstup != 0)
+                {
+                    nejmensi = vstup;
+                }
+
+                if (vstup % 2 == 0 && vstup != 0)
+                {
+                    pocetdsudych++;
+                } else if (vstup != 0)
+                {
+                    pocetlich++;
+                }
+            } while (vstup != 0);
+
+            Console.WriteLine($"Největší číslo je {nejvetsi}! \nNejmenší číslo je {nejmensi}.\nPočet sudých čísel je {pocetdsudych}.\nPočet lichých čísel je {pocetlich}.");
+        }
+    }
+}
---
+++ 03_Cyklus_While/03_Cvic_Nakup.cs
@@ -0,0 +1,50 @@
+﻿namespace nakup
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int pocet = 0;
+            double cel_cena = 0;
+            int vyber = 0;
+            int kusy = 0;
+
+            do
+            {
+                Console.WriteLine("Pro výběr zboží stiskni příslušné číslo: houska (1); rohlík (2); chleba (3). Pro ukončení zadej 0!");
+                while (!int.TryParse(Console.ReadLine(), out vyber))
+                {
+                    Console.WriteLine("Zadej číslo!");
+                }
+                Console.WriteLine("Kolik kusů daného zboží chceš?");
+                while (!int.TryParse(Console.ReadLine(), out kusy))
+                    {
+                        Console.WriteLine("Zadej číslo!");
+                    }
+                switch (vyber)
+                {
+                    case 1:
+                        cel_cena += (5.5*kusy);
+                        pocet += kusy;
+                        break;
+                    case 2:
+                        cel_cena += (3.5*kusy);
+                        pocet += kusy;
+                        break;
+                    case 3:
+                        cel_cena += (10*kusy);
+                        pocet += kusy;
+                        break;
+                    case 0:
+                        Console.WriteLine("Nákup byl ukončen!");
+                        break;
+                    default:
+                        Console.WriteLine("Byl zadán neplatný vstup");
+                        break;
+                }
+            } while (pocet <= 10 && vyber != 0);
+
+            Console.WriteLine($"Celková cena nákup je {cel_cena} a bylo nakoupeno {pocet} kus/ů/y!");
+        }
+    }
+}
