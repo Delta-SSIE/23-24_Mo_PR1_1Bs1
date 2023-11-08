cde19345e08485a01133db98993f6688279a953a
Jan Mottl <mottja@delta-skola.cz>
2023-11-08 11:59:53 +0100

    ok

---
+++ 04_Cyklus_For/04_UkolRep_Obracene_slovo.cs
@@ -0,0 +1,21 @@
+﻿namespace obracene_slovo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string slovo = "kanárek";
+
+            /*Na začátku i nabude hodnoty délky textového řetězce 'slovo'.
+             *Délka řetězce se udává v počtu jeho znaků a jelikož indexování začíná od nuly. Tak slovo kanárek má sice 7 znaků ale má indexy 0–6, proto musíme na začátku délku o 1 snížit.
+             *Následně se kontrolu zda i je větší než -1, protože nulu ještě musíme nechat projít.
+             *Po každém průchodu cyklu se i o 1 snižuje.
+             */
+            for (int i = slovo.Length-1; i > -1; i--)
+            {
+                Console.WriteLine(slovo[i]);
+            }
+        }
+    }
+}
