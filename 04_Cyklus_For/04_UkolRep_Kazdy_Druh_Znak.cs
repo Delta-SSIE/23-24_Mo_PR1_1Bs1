7f0a1253154c083851add521bc76a941b891a22e
Jan Mottl <mottja@delta-skola.cz>
2023-11-08 12:15:19 +0100

    ok

---
+++ 04_Cyklus_For/04_UkolRep_Kazdy_Druh_Znak.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "zadřeš on i co onsemnaí";
+
+            /*
+             * Podmínka je nastavena na délku řetězce a jelikož je počet znaků o 1 větší než indexování znaků, které začíná 0, pak nám podmínka funguje.
+             * Po každén průchodu cyklem zvyšuji i o 2 a díky tomu vypisuji pouze každý druhý znak.
+             */
+            for (int i = 0; i < text.Length; i+=2)
+            {
+                Console.WriteLine(text[i]);
+            }
+
+        }
+    }
+}
