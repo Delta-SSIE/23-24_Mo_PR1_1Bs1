66f9476b7dc98903d1a93ac15d764485c03fccfe
Jan Mottl <mottja@delta-skola.cz>
2023-11-13 09:50:10 +0100

    ok

---
+++ 04_Cyklus_For/04_Prevod_cisla_na_znak.cs
@@ -0,0 +1,23 @@
+﻿namespace prevod_cislo_znak
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Základní slovo ze kterého se vyberou znaky, které se budou převádět na čísla.
+            string slovo = "Ahoj";
+
+            //Konvertování znaku na 1 indexu v stringu na číslo.
+            int a = Convert.ToInt32(slovo[1]);
+
+            //Výpis čísla
+            Console.WriteLine(a);
+
+            //Konvertování čísla na char
+            char b = Convert.ToChar(a);
+
+            //Výpis znaku
+            Console.WriteLine(b);
+        }
+    }
+}
