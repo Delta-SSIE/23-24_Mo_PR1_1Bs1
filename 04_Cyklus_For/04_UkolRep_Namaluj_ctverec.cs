06945cc1cfccd1506bee0befe08ef77beb261042
Jan Mottl <mottja@delta-skola.cz>
2023-11-13 10:16:27 +0100

    ok

---
+++ 04_Cyklus_For/04_UkolRep_Namaluj_ctverec.cs
@@ -0,0 +1,20 @@
+﻿namespace namaluj_ctverec
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Napiš mi délku stran čtverce");
+            int strana = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < strana; i++)
+            {
+                for (int n = 0; n < strana; n++)
+                {
+                    Console.Write('#');
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
