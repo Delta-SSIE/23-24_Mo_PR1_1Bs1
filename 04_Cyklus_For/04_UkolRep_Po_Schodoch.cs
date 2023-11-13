edbc03e5898db1c22c0a87a9a2fb6d08d8e3e4a2
Jan Mottl <mottja@delta-skola.cz>
2023-11-13 09:19:49 +0100

    ok

---
+++ 04_Cyklus_For/04_UkolRep_Po_Schodoch.cs
@@ -0,0 +1,29 @@
+﻿namespace po_schodoch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int patra;
+
+            Console.WriteLine("Zadej počet pater");
+            
+            while(!int.TryParse(Console.ReadLine(),out patra))
+            {
+                Console.WriteLine("Zadej číslo!");
+            }
+
+            Console.Clear();
+
+            for (int i = 1; i <= patra; i++)
+            {
+                for (int n = 0; n < i; n++)
+                {
+                    Console.Write("#");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
