8f2668e4b4014ffba7dda44718a8038f15edc9bc
Jan Mottl <mottja@delta-skola.cz>
2023-11-15 12:25:41 +0100

    ok

---
+++ 04_Cyklus_For/04_UkolRep_Trojuhelnik_p.cs
@@ -0,0 +1,23 @@
+﻿namespace trojuhelník_p
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int strana = 10;
+
+            //Vnější for cyklus pro vypisování řádků. Počet řádků je určen proměnou strana.
+            for (int i = 0; i < strana; i++)
+            {
+                //Vnitřní for cyklus, který slouží pro vypisování sloupců (počet # v rámci řádku). S každým dalším řádkem bude o jeden # méně, proto se ověřuje strana - i, neboli strana mínus počet řádků.
+                for (int j = 0; j < strana-i; j++)
+                {
+                    Console.Write('#');
+                }
+                //zalomení a konci řádku
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
