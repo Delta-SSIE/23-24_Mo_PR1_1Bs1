01899e12e6bebb94f4de918b19cc6123922ad0a7
Jan Mottl <mottja@delta-skola.cz>
2023-11-20 10:23:29 +0100

    ok

---
+++ 04_Cyklus_For/04_UkolRep_Fibonnacci.cs
@@ -0,0 +1,26 @@
+﻿namespace fibonnacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.Write("Kolik Fibonacciho čísel chceš spočítat: ");
+            n = int.Parse(Console.ReadLine());
+
+            int predchozi = 1;
+            int cislo = 1;
+            int starypredchozi;
+
+            Console.Write("1 1 ");
+            for (int i = 3; i < n; i++)
+            {
+                starypredchozi = predchozi;
+                predchozi = cislo;
+                cislo = predchozi + starypredchozi;
+                Console.Write(cislo + " ");
+                
+            }
+        }
+    }
+}
