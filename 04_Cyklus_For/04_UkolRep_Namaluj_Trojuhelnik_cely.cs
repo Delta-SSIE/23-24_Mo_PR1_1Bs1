13696eaad1ec4cdd9f64f844dd49fc3736e31b4b
Jan Mottl <mottja@delta-skola.cz>
2023-11-20 09:23:53 +0100

    ok

---
+++ 04_Cyklus_For/04_UkolRep_Namaluj_Trojuhelnik_cely.cs
@@ -0,0 +1,46 @@
+﻿namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int vyska = 20;
+
+            //Vnější for cyklus na vypsání řádků (výšky)
+            for (int i = 0; i < vyska; i++)
+            {
+                //For cyklus vnitřní na řešení levé strany trojuhelníku
+                for (int j = 0; j < vyska; j++)
+                {
+                    if(j+1 > vyska - i)
+                    {
+                        Console.Write('#');
+                    } else
+                    {
+                        Console.Write(' ');
+                    }
+                    
+                }
+
+                //Vnitřní linka trojuhelníku
+                Console.Write('#');
+
+                //For cyklus vnitřní na řešení pravé strany trojuhelníku
+                for (int n = 0; n < vyska; n++)
+                {
+                   if (n < i)
+                    {
+                        Console.Write('#');
+                    } else
+                    {
+                        Console.Write(' ');
+                    }
+                }
+
+                //Zalomení řádku
+                Console.WriteLine();
+
+            }
+        }
+    }
+}
