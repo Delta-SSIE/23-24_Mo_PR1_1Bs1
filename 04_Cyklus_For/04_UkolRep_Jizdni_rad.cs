188ed9a3641745f819d00b5dfbc2fd46e629f09a
Jan Mottl <mottja@delta-skola.cz>
2023-11-20 09:57:25 +0100

    ok

---
+++ 04_Cyklus_For/04_UkolRep_Jizdni_rad.cs
@@ -0,0 +1,41 @@
+﻿namespace jizdni_rad
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int hodiny = 5;
+            int minuty = 42;
+
+            for (int i = 0; i < 21; i++)
+            {
+                //Výpis, aby byl s nulou, pokud je hodin méně než 10
+                if (hodiny < 10)
+                {
+                    Console.Write($"Autobus jede v 0{hodiny}:");
+                } else
+                {
+                    Console.Write($"Autobus jede v {hodiny}:");
+                }
+                //Výpis, aby byl s nulou, pokud je minut méně než 10
+                if (minuty < 10)
+                {
+                    Console.WriteLine($"0{minuty}");
+                }
+                else
+                {
+                    Console.WriteLine($"{minuty}");
+                }
+
+                //Zvýšení minut o 13, což je interval autobusů
+                minuty += 13;
+                //Pokud je minut více, či rovno 60, pak je třeba je převést na hodiny.
+                if (minuty >= 60)
+                {
+                    minuty -= 60;
+                    hodiny++;
+                }
+            }
+        }
+    }
+}
