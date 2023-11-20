ef043c55145e3fb03aaaaf02d5d19e2340d965db
Jan Mottl <mottja@delta-skola.cz>
2023-11-20 10:07:20 +0100

    ok

---
+++ 04_Cyklus_For/04_UkolRep_Kostky_v_pyramide.cs
@@ -0,0 +1,20 @@
+﻿namespace pyramida
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int vyska = 25;
+            int pocet = 0;
+            //dál pokračujte vy
+
+            for (int i = 1; i <= vyska; i++)
+            {
+                pocet += i * i;
+            }
+
+            //zde končí váš kód
+            Console.WriteLine($"Na stavbu pyramidy výšky {vyska} je potřeba {pocet} kostek.");
+        }
+    }
+}
