9e3e22146c64b942f3df06c45bb542c068ca80d9
Jan Mottl <mottja@delta-skola.cz>
2023-11-29 12:58:08 +0100

    ok

---
+++ 05_Pole/05_Balicek_Karet_Urove_1.cs
@@ -0,0 +1,28 @@
+﻿namespace balicek_karet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] barvy = { "srdce", "kule", "žaludy", "listy" };
+            string[] vysky = { "sedma", "osma", "devítka", "desítka", "spodek", "svršek", "král", "eso" };
+
+            string[] balicek = new string[32];
+
+            int pocetkaret = 0;
+            for (int i = 0; i < barvy.Length; i++)
+            {
+                for (int n = 0; n < vysky.Length; n++)
+                {
+                    balicek[pocetkaret] = barvy[i] + " - " + vysky[n];
+                        pocetkaret++;
+                }
+            }
+
+            foreach (string karta in balicek)
+            {
+                Console.WriteLine(karta);
+            }
+        }
+    }
+}
