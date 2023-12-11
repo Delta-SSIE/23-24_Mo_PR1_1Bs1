b8f8ea838145f6c5dbb05a3737709dbcc110d2da
Jan Mottl <mottja@delta-skola.cz>
2023-12-11 10:15:06 +0100

    ok

---
+++ 05_Pole/05_Cvic_Dny_v_roce.cs
@@ -0,0 +1,26 @@
+﻿namespace Dny_v_roce
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] dny = { "pondělí", "úterý", "středa", "čtvrtek", "pátek", "sobota", "neděle" };
+            string[] mesice = { "leden", "únor", "březen", "duben", "květen", "červen", "červenec", "srpen", "září", "říjen", "listopad", "prosinec" };
+            int pocitadlo_den = 0;
+
+            for (int i = 0; i < mesice.Length; i++)
+            {
+                for (int n = 1; n < 31; n++)
+                {
+                    if (pocitadlo_den == 7)
+                    {
+                        pocitadlo_den = 0;
+                    }
+                    Console.WriteLine($"{n}-{dny[pocitadlo_den]}-{mesice[i]}");
+                    pocitadlo_den++;
+                }
+
+            }
+        }
+    }
+}
---
+++ 05_Pole/05_Cvic_Sazkovy_ticket.cs
@@ -0,0 +1,24 @@
+﻿namespace Sazkovy_ticket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sportka = new int[] { 9, 5, 6, 1, 5, 6 };
+            int uhadlacisla = 0;
+
+            for (int i = 0; i < sportka.Length; i++)
+            {
+                Console.WriteLine($"Zadej mi {i+1}.číslo");
+                int zadanecislo = int.Parse(Console.ReadLine());
+
+                if (sportka[i] == zadanecislo)
+                {
+                    uhadlacisla++;
+                }
+            }
+
+            Console.WriteLine($"Výhra je {uhadlacisla*10*uhadlacisla}");
+        }
+    }
+}
