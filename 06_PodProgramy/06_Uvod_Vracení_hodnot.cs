4b191fc0ef5ff2bc31ff14147a8ff6d191eaf742
Jan Mottl <mottja@delta-skola.cz>
2024-01-03 12:05:21 +0100

    ok

---
+++ 06_PodProgramy/06_Uvod_Vracení_hodnot.cs
@@ -0,0 +1,28 @@
+﻿namespace kamaradi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Metoda NactiCislo vrací int, který se uloží do proměných
+            int a = NactiCislo();
+            int b = NactiCislo();
+
+            Console.WriteLine(a+b);
+
+
+        }
+
+        static int NactiCislo()
+        {
+            int cislo = 0;
+            Console.WriteLine("Napiš mi číslo");
+            while (!int.TryParse(Console.ReadLine(), out cislo))
+            {
+                Console.WriteLine("Tohle není číslo, zkus to znovu!");
+            }
+            //vrácení hodnoty uložené v proměné číslo
+            return cislo;
+        }
+    }
+}
