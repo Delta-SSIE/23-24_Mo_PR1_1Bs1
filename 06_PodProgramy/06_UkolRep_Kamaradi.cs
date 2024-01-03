8e4c3e32735290b6ab892296c922c21754f5bc51
Jan Mottl <mottja@delta-skola.cz>
2024-01-03 12:09:14 +0100

    ok

---
+++ 06_PodProgramy/06_UkolRep_Kamaradi.cs
@@ -0,0 +1,19 @@
+﻿namespace kamaradi_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Volání metody "Kamaradi" s dvěma argumenty
+            Kamaradi("Anička", "Karel");
+            Kamaradi("Blažej", "Jana");
+            Kamaradi("Ruprecht", "Ignác");
+        }
+
+        //Metoda "Kamaradi", která očekává dva argumenty, které naplní parametry "kamarad1" a "kamarad2"
+        static void Kamaradi(string kamarad1, string kamarad2)
+        {
+            Console.WriteLine($"Moji kamarádi jsou {kamarad1} a {kamarad2}");
+        }
+    }
+}
