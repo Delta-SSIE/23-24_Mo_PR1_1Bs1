25e2b5ea2d265b0c6ab917dd77ecb4e2f814cb97
Jan Mottl <mottja@delta-skola.cz>
2024-01-03 12:41:05 +0100

    ok

---
+++ 06_PodProgramy/06_UkolRep_Opis_stokrat.cs
@@ -0,0 +1,20 @@
+﻿namespace opis_stokrat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //metoda "Opisvetu" dostane dva argumenty - int a string
+            Opisvetu(10, "Nauč se programovat!");
+        }
+
+        //Metoda očekává dva parametry (int a string)
+        static void Opisvetu(int n, string veta)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(veta);
+            }
+        }
+    }
+}
