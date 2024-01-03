19d9f1272b201c447af869147e81ebd0069153b4
Jan Mottl <mottja@delta-skola.cz>
2024-01-03 12:52:14 +0100

    ok

---
+++ 06_PodProgramy/06_UkolRep_Hodnoty_pod_limitem.cs
@@ -0,0 +1,26 @@
+﻿namespace hod_pod_lim
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] cisla = { -1.3, 2.2, 1.5, 1.4, 2.7 };
+            Console.WriteLine(PodLimitem(cisla, 1.5)); //vypíše -1.3 a 1.4 
+            Console.WriteLine(PodLimitem(cisla, 1.6)); //vypíše -1.3 , 1.5 a 1.4
+        }
+
+        static string PodLimitem(double[] pole, double limit)
+        {
+            string vypis = "";
+            foreach(double cislo in pole)
+            {
+                if (cislo < limit)
+                {
+                    vypis = vypis + " " + cislo;
+                }
+            }
+            return vypis;
+        }
+
+    }
+}
