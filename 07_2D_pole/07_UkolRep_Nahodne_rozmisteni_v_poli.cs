5e2e05270e6c42b5929699158689fcc5cb128d3e
Jan Mottl <mottja@delta-skola.cz>
2024-02-07 12:23:59 +0100

    ok

---
+++ 07_2D_pole/07_UkolRep_Nahodne_rozmisteni_v_poli.cs
@@ -0,0 +1,56 @@
+﻿namespace Nahodne_pole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            VykresliPole(VytvorMapu(5, 10, 30));
+        }
+        static void VykresliPole(bool[,] mapa, string pravda = "#", string nepravda = " ")
+        {
+            for (int i = 0; i < mapa.GetLength(0); i++)
+            {
+                for (int j = 0; j < mapa.GetLength(1); j++)
+                {
+                    if (mapa[i, j] == true)
+                    {
+                        Console.Write(pravda);
+                    }
+                    else
+                    {
+                        Console.Write(nepravda);
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+        static bool[,] VytvorMapu(int vyska, int sirka, int pocetminci)
+        {
+            Random generator = new Random();
+
+            bool[,] pole = new bool[vyska, sirka];
+
+            for (int i = 0; i < pole.GetLength(0); i++)
+            {
+                for (int j = 0; j < pole.GetLength(1); j++)
+                {
+                    pole[i, j] = false;
+                }
+            }
+
+            for (int i = 0; i < pocetminci; i++)
+            {
+                int nah_vyska = generator.Next(0, vyska);
+                int nah_sirka = generator.Next(0, sirka);
+                while (pole[nah_vyska, nah_sirka] == true)
+                {
+                        nah_vyska = generator.Next(0, vyska);
+                    nah_sirka = generator.Next(0, sirka);
+                }
+                pole[nah_vyska, nah_sirka] = true;
+            }
+
+            return pole;
+        }
+    }
+}
