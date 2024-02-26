9bdef100c0052bd8248da31478e12fb719327043
Jan Mottl <mottja@delta-skola.cz>
2024-02-26 10:25:58 +0100

    ok

---
+++ 09_OOP/09_UkolRep_Enumerace_karty.cs
@@ -0,0 +1,52 @@
+﻿namespace karty_enum
+{
+    internal class Program
+    {
+        enum Karta
+        {
+            sedma = 7,
+            osma,
+            devítka,
+            desítka,
+            spodek,
+            filek = 11,
+            svršek,
+            královna,
+            eso
+        }
+        static void Main(string[] args)
+        {
+            Karta moje_karta = Karta.desítka;
+            Console.WriteLine(moje_karta);
+
+            moje_karta++;
+            Console.WriteLine(moje_karta);
+
+            moje_karta = Karta.eso;
+            moje_karta++;
+            Console.WriteLine(moje_karta);
+
+            Karta A = Karta.eso;
+            Karta B = Karta.eso;
+
+            KdoJeVyssi(A, B);
+
+        }
+
+        static void KdoJeVyssi( Karta kartaA, Karta kartaB)
+        {
+            int a = (int) kartaA;
+            int b = (int) kartaB;
+
+            if (a == b)
+            {
+                Console.WriteLine("Karty mají stejnou hodnotu!");
+            } else if (a > b) {
+                Console.WriteLine($"{kartaA} je vyšší než {kartaB}");
+            } else
+            {
+                Console.WriteLine($"{kartaB} je vyšší než {kartaA}");
+            }
+        }
+    }
+}
