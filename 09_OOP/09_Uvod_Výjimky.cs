249bc582f09edad33b9f0888c9ea13cda1989f96
Jan Mottl <mottja@delta-skola.cz>
2024-03-04 08:53:33 +0100

    ok

---
+++ 09_OOP/09_Uvod_Výjimky.cs
@@ -0,0 +1,29 @@
+﻿namespace vyjimka
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] banka = { 120, 50, 120, 50, 150 };
+            Console.WriteLine("Zadej mi index tvého účtu!");
+            int i = 0;
+
+            try
+            {
+                i = int.Parse(Console.ReadLine());
+            } catch
+            {
+                Console.WriteLine("Zadal jsi špatný datový typ!");
+            }
+
+            try
+            {
+                Console.WriteLine(banka[i]);
+            } catch
+            {
+                i = banka.Length - 1;
+                Console.WriteLine(banka[i]);
+            }
+        }
+    }
+}
