b446f2e9e454d87604e6bf5a7cb690b9b238b098
Jan Mottl <mottja@delta-skola.cz>
2024-03-04 10:25:04 +0100

    ok

---
+++ 09_OOP/09_UkolRep_Pes/Pes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Pes
+{
+	//Vlastnosti (položky datové)
+	public string Jmeno;
+	public int Nohy;
+	public bool JeOckovany;
+
+	//Metoda
+	public string PredstavSe()
+	{
+		//Naplnění proměnné očkovani pomocí short-hand if.
+		string ockovani = (JeOckovany) ? "očkovaný" : "neočkovaný";
+        string text = $"Já jsem pes, jmenuju se {Jmeno}, mám {Nohy} nohy a jsem {ockovani}.";
+		return text;
+	}
+}
