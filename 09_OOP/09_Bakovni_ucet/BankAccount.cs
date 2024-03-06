af653550a8db9a755c824554cd872b921dc455ac
Jan Mottl <mottja@delta-skola.cz>
2024-03-06 12:57:03 +0100

    ok

---
+++ 09_OOP/09_Bakovni_ucet/BankAccount.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public class BankAccount
+{
+    public int AccountNumber;
+    public string AccountHolderName;
+    public double AccountBalance;
+    public string AccountType;
+  
+    public BankAccount(int cislo, string jmeno, double castka, string typ)
+    {
+        this.AccountNumber = cislo;
+        AccountHolderName = jmeno;
+        AccountBalance = castka;
+        AccountType = typ;
+    }
+
+    public void Deposit(double castka)
+    {
+        AccountBalance += castka;
+        Console.WriteLine($"Na účtu je nyní {AccountBalance} zůstatek!");
+    }
+
+    public void Withdrawal(double castka)
+    {
+        if(AccountBalance < castka)
+        {
+            Console.WriteLine("Nemáš dostatek peněz na účtu!");
+        } else
+        {
+            AccountBalance -= castka;
+            Console.WriteLine($"Na účtu je nyní {AccountBalance} zůstatek!");
+        }
+    }
+}
---
+++ 09_OOP/09_Bakovni_ucet/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace banka
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount muj_ucet = new BankAccount(10, "Jan", 200, "gold");
+            int volba = 0;
+            double suma = 0;
+
+            while (true)
+            {
+                Console.WriteLine("Vítej ve správě svého účtu \nPro výběr dej 1 Pro vklad dej 2");
+                try
+                {
+                    volba = int.Parse(Console.ReadLine());
+                } catch
+                {
+                    Console.WriteLine("Zadal jsi nesmysl");
+                }
+                switch (volba)
+                {
+                    case 1:
+                        Console.WriteLine("Zadej sumu, co chceš vybrat");
+                        try
+                        {
+                            suma = double.Parse(Console.ReadLine());
+                        }
+                        catch
+                        {
+                            Console.WriteLine("Zadal jsi nesmysl");
+                        }
+                        muj_ucet.Withdrawal(suma);
+                        break;
+                    case 2:
+                        Console.WriteLine("Zadej sumu, co chceš vložit");
+                        try
+                        {
+                            suma = double.Parse(Console.ReadLine());
+                        }
+                        catch
+                        {
+                            Console.WriteLine("Zadal jsi nesmysl");
+                        }
+                        muj_ucet.Deposit(suma);
+                        break;
+
+                }
+            }
+        }
+    }
+}
