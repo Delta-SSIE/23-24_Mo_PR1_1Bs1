b2a8c0726b2a23fbaa2fd0b0e862c0d664014f6b
Jan Mottl <mottja@delta-skola.cz>
2024-03-06 11:46:51 +0100

    ok

---
+++ 09_OOP/09_Uvod_Opakovani_Human/Human.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class Human
+{
+    //Vlastnosti/Atributy/Položky
+    public string Jmeno;
+    public int PocetKoncetin = 4;
+    public int Vek;
+
+    //Konstruktor
+    public Human(string name, int vek)
+	{
+        Jmeno = name;
+        Vek = vek;
+	}
+
+    //Metody
+
+    public void PredstavSe()
+    {
+        Console.WriteLine($"Ahoj, já jsem {Jmeno} a je mi {Vek} let! A mimochodem mám {PocetKoncetin} končetin!");
+    }
+
+    public bool MuzeChodit()
+    {
+        if (PocetKoncetin <= 1)
+        {
+            return false;
+        } else
+        {
+            return true;
+        }
+    }
+
+	
+
+}
---
+++ 09_OOP/09_Uvod_Opakovani_Human/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace OOP_opakovani
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Deklarace nového objektu
+            Human pepa = new Human("Pepa", 25);
+            Human lukas = new Human("Lukáš", 31);
+
+            //Volání metody daného objektu
+            pepa.PredstavSe();
+            lukas.PredstavSe();
+
+            //Změna vlastnost
+            pepa.PocetKoncetin = 0;
+            pepa.PredstavSe();
+
+            Console.WriteLine(pepa.MuzeChodit());
+            Console.WriteLine(lukas.MuzeChodit());
+        }
+    }
+}
