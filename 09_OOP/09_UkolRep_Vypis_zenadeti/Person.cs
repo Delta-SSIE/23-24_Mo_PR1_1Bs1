d89623b2a90b4b91ff87c22450274f77e90cc11b
Jan Mottl <mottja@delta-skola.cz>
2024-03-06 12:28:41 +0100

    ok

---
+++ 09_OOP/09_UkolRep_Vypis_zenadeti/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+enum SexType : byte { Male, Female }
+
+class Person
+{
+    public string Name;
+    public string Surname;
+    public int Age;
+    public SexType Sex;
+
+    public string IntroduceSelf()
+    {
+        string text = $"{Name}, {Surname}, ({Sex}, {Age} years)";
+        return text;
+    }
+}
---
+++ 09_OOP/09_UkolRep_Vypis_zenadeti/Program.cs
@@ -0,0 +1,100 @@
+﻿namespace Vypis_zen_a_deti
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //vytvoříme jeden zkušební objekt
+            Person tom = new Person
+            {
+                Name = "Tom",
+                Surname = "Riddle",
+                Age = 9,
+                Sex = SexType.Male
+            };
+
+            //ověříme, že jsme správně zapsali jeho představení
+            Console.WriteLine(tom.IntroduceSelf());
+
+            // nyní připravíme pole osob
+            Person[] friends = new Person[]
+            {
+                new Person {
+                    Name = "Thomas",
+                    Surname = "Carter",
+                    Age = 17,
+                    Sex = SexType.Male
+                },
+                new Person {
+                    Name = "Lillian",
+                    Surname = "Carter",
+                    Age = 19,
+                    Sex = SexType.Female
+                },
+                new Person {
+                    Name = "Jim",
+                    Surname = "Pearson",
+                    Age = 54,
+                    Sex = SexType.Male
+                },
+                new Person {
+                    Name = "Lou",
+                    Surname = "Lacey",
+                    Age = 36,
+                    Sex = SexType.Female
+                },
+                new Person {
+                    Name = "Minnie",
+                    Surname = "Stanfords",
+                    Age = 84,
+                    Sex = SexType.Female
+                },
+                new Person {
+                    Name = "Quincy",
+                    Surname = "Dean",
+                    Age = 12,
+                    Sex = SexType.Male
+                },
+                new Person {
+                    Name = "Steve",
+                    Surname = "Blake",
+                    Age = 61,
+                    Sex = SexType.Male
+                },
+                new Person {
+                    Name = "Andrew",
+                    Surname = "Holmes",
+                    Age = 33,
+                    Sex = SexType.Male
+                }
+            };
+
+            Console.WriteLine();
+            FindKids(friends);
+            Console.WriteLine();
+            FindWomen(friends);
+
+        }
+        public static void FindKids(Person[] list)
+        {
+            for (int i = 0; i < list.Length; i++)
+            {
+                if (list[i].Age < 18)
+                {
+                    Console.WriteLine(list[i].IntroduceSelf());
+                }
+            }
+        }
+
+        public static void FindWomen(Person[] list)
+        {
+            foreach (Person osoba in list)
+            {
+                if (osoba.Sex == SexType.Female)
+                {
+                    Console.WriteLine(osoba.IntroduceSelf());
+                }
+            }
+        }
+    }
+}
