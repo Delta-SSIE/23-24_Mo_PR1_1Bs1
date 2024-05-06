7b76e04d9f58b81e6075b79e612a6cec44cd6094
Jan Mottl <mottja@delta-skola.cz>
2024-05-06 09:12:11 +0200

    kk

---
+++ 10_Doprovodna_Temata/10_Fronta.cs
@@ -0,0 +1,54 @@
+﻿namespace fronta
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] courses = { "MCA", "MBA", "BCA", "BBA", "BTech", "MTech" };
+            //Incializace fronty s prvky z pole
+            Queue<string> queue = new Queue<string>(courses);
+
+            //Vypsání fronty
+            foreach (string prvek in queue)
+            {
+                Console.WriteLine(prvek);
+            }
+
+            //Výpis počtu prvků z fronty
+            Console.WriteLine(queue.Count());
+
+            //Přidání prvku do fronty
+            Queue<int> frontaCis = new Queue<int>();
+            frontaCis.Enqueue(1);
+            frontaCis.Enqueue(2);
+            frontaCis.Enqueue(3);
+            frontaCis.Enqueue(4);
+            frontaCis.Enqueue(5);
+            frontaCis.Enqueue(6);
+
+            foreach (int prvek in frontaCis)
+            {
+                Console.WriteLine(prvek);
+            }
+
+            //Vyhození prvků z fronty (odebírá se od prvního prvku ve frontě)
+            frontaCis.Dequeue();
+            frontaCis.Dequeue();
+
+            foreach (int prvek in frontaCis)
+            {
+                Console.WriteLine(prvek);
+            }
+
+            //Hledání ve frontě
+            Console.WriteLine(frontaCis.Contains(3));
+            Console.WriteLine(frontaCis.Contains(1));
+
+            //Peek
+            Console.WriteLine(frontaCis.Peek());
+            Console.WriteLine(queue.Peek());
+
+
+        }
+    }
+}
---
+++ 10_Doprovodna_Temata/10_Zasobnik.cs
@@ -0,0 +1,59 @@
+﻿namespace zasobnik
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] str = {
+                "MCA",
+                "BCA",
+                "BBA",
+                "MBA",
+                "MTech"
+            };
+
+            //Incializace zásobníku naplněného prvky z pole
+            Stack<string> zasobnik_Jmen = new Stack<string>(str);
+
+            //Výpis počtu prvků v zásobníku
+            Console.WriteLine(zasobnik_Jmen.Count());
+
+            //Výpis zásobniku
+            foreach (string prvek in zasobnik_Jmen)
+            {
+                Console.Write(prvek + " ");
+            }
+            Console.WriteLine();
+
+            //Inicializace prázdného zásobníku
+            Stack<string> zasAdres = new Stack<string>();
+
+            //Vkládání prvků do zásobníku
+            zasAdres.Push("Kamenická 123");
+            zasAdres.Push("Lidická 123");
+            zasAdres.Push("Faustova 666");
+
+            foreach (string prvek in zasAdres)
+            {
+                Console.WriteLine(prvek + " ");
+            }
+
+            //Odstranění prvků ze zásobníku (vyhazují se prvky od posledního přidaného)
+            zasAdres.Pop();
+            zasAdres.Pop();
+            foreach (string prvek in zasAdres)
+            {
+                Console.WriteLine(prvek + " ");
+            }
+
+            //Zjištění, co je první prvek v zásobníku
+            Console.WriteLine(zasAdres.Peek());
+            Console.WriteLine(zasobnik_Jmen.Peek());
+
+            //Smazaní všech prvků v zásobníku
+            zasAdres.Clear();
+
+
+        }
+    }
+}
