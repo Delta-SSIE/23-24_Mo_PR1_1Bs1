c64fb4b3744adf96fd22d91c962feb82d76134cd
Jan Mottl <mottja@delta-skola.cz>
2024-06-03 09:03:42 +0200

    kk

---
+++ 10_Doprovodna_Temata/10_Directory.cs
@@ -0,0 +1,19 @@
+﻿namespace Adresar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Tvorba adresáře
+            //relativní cesta od .exe souboru
+            Directory.CreateDirectory(@"adresar");
+            //absolutní cesta k adresáři
+            Directory.CreateDirectory(@"C:\Users\mottja\Desktop\adresar");
+
+            if (!Directory.Exists(@"slozka"))
+            {
+                Directory.CreateDirectory(@"slozka");
+            }
+        }
+    }
+}
